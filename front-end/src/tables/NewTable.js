e39b01abb39f06db623b6b34f7ffb03e2177b305
Lucy Maneiro <lucybarnett.ot@gmail.com>
2021-10-09 14:15:32 -0500

    attempted merge to main branch

---
+++ front-end/src/tables/NewTable.js
@@ -0,0 +1,67 @@
+import React, {useState} from "react";
+import {useHistory} from "react-router-dom"
+
+function NewTable(){
+    const initialFormState = {
+        table_name: "",
+        capacity: 1,
+    }
+
+    const [tableForm, setTableForm] = useState({...initialFormState})
+    const [tableFormErrors, setTableFormErrors] = useState([])
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        
+
+    }
+
+    const handleChange = ({target}) => {
+        let value = target.value
+        let name = target.name
+
+        setTableForm({
+            ...tableForm,
+            [value]: name,
+        })
+
+
+    }
+
+    return (
+        <div>
+            <h1>New Table</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="container">
+                    <div className="form-group">
+                        <label htmlFor="table_name">Table Name</label>
+                            <input 
+                               type="text"
+                               id="table_name"
+                               name="table_name"
+                               placeholder="Table name"
+                               required = {true}
+                               onChange = {handleChange} 
+                            />
+                    </div>
+                
+                    <div className = "form-group">
+                        <label htmlFor="capacity">Capacity</label>
+                            <input
+                                type = "number"
+                                name= "capacity"
+                                id="capacity"
+                                placeholder = "Enter capacity here"
+                                required = {true}
+                                onChange = {handleChange}
+                            />
+                    </div>
+                </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="button" className="btn btn-secondary" onClick={()=> history.goBack()}>Cancel</button>
+            </form>
+        </div>
+    )
+}
+
+export default NewTable;
