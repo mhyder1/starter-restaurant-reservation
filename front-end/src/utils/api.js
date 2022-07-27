9a29ed22e24ffef792b1e5ae067e6548d3204748
unknown <hyder1@gmail.com>
2022-07-27 17:03:20 -0400

    updates

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -7,7 +7,8 @@
  import formatReservationDate from "./format-reservation-date";
  import formatReservationTime from "./format-reservation-date";
  
- const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+ const API_BASE_URL = 'https://hyder-backend.herokuapp.com'
+//  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
  
  /**
   * 
